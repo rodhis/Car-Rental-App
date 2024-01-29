bd8fc9355eaf83a4e099d7e0cbac390b4a806418
rodhis <rodrigonoliva@gmail.com>
2024-01-29 19:25:15 -0300

    started footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,7 +8,22 @@ import styles from "./Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <img src={logo} alt="locofy logo" />
+      <div className={styles.footerLinks}>
+        <img src={logo} alt="locofy logo" />
+        <p>MyRide Inc., 2nd Floor, New York, NY 10016</p>
+        <div className={styles.social}>
+          <img src={facebook} alt="facebook logo" />
+          <img src={instagram} alt="instagram logo" />
+          <img src={twitter} alt="twitter logo" />
+        </div>
+      </div>
+      <div className={styles.footerMenu}>
+        <h1>Company</h1>
+        <p>About Us</p>
+        <p>News</p>
+        <p>Career</p>
+        <p>How we Work</p>
+      </div>
     </footer>
   );
 }
