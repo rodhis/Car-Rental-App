302af10eb8b080a0410d43eb1023499632eb82bf
Rodhis <rodrigonoliva@gmail.com>
2024-01-29 22:15:53 -0300

    starting mui form

--- src/Pages/Home/DriverForm/DriverForm.jsx
+++ src/Pages/Home/DriverForm/DriverForm.jsx
@@ -6,15 +6,25 @@ import styles from "./DriverForm.module.css";
 
 export default function DriverForm() {
   return (
-    <div className={styles.formHeader}>
-      <img src={formImage} alt="driver form car image" />
-      <div className={styles.headerText}>
-        <h1>Drive with MyRide</h1>
-        <p>
-          Register as a driver using the form below. Our team will assess and
-          get back to you within 48 hours.
-        </p>
+    <>
+      <div className={styles.formHeader}>
+        <img src={formImage} alt="driver form car image" />
+        <div className={styles.headerText}>
+          <h1>Drive with MyRide</h1>
+          <p>
+            Register as a driver using the form below. Our team will assess and
+            get back to you within 48 hours.
+          </p>
+        </div>
       </div>
-    </div>
+      <div className={styles.formBody}>
+        <TextField
+          id="outlined-basic"
+          label="Outlined"
+          variant="outlined"
+          defaultValue="Hello World"
+        />
+      </div>
+    </>
   );
 }
