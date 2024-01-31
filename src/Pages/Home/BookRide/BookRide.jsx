b44b2aaca3a7a8aee3ac9088ae60f3f339ddc2fc
Rodhis <rodrigonoliva@gmail.com>
2024-01-30 22:06:02 -0300

    finished BookRide component

--- src/Pages/Home/BookRide/BookRide.jsx
+++ src/Pages/Home/BookRide/BookRide.jsx
@@ -1,11 +1,16 @@
-import { Container, ThemeProvider } from "@mui/material";
+import { Container, ThemeProvider, TextField, Button } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 
 import bookRideImg from "../../../assets/img/book-ride.svg";
 
 import { bookRideTheme } from "./bookRideTheme";
 import styles from "./BookRide.module.css";
+import { useState } from "react";
 
 export default function BookRide() {
+  const [location, setLocation] = useState("Current location");
+  const [destination, setDestination] = useState(" ");
+
   return (
     <div className={styles.bookRide}>
       <img src={bookRideImg} alt="Book myRide image" />
@@ -14,10 +19,34 @@ export default function BookRide() {
         <h1>
           Book with <span>MyRIDE</span> now!
         </h1>
-      </div>
-      <div>
         <ThemeProvider theme={bookRideTheme}>
-          <Container></Container>
+          <Container>
+            <p className={styles.findMyRideText}>Find a Ride Now</p>
+            <TextField
+              fullWidth
+              IconProps={{ style: { fill: bookRideTheme.palette.icon.main } }}
+              id="location"
+              variant="outlined"
+              label="Your Pickup"
+              value={location}
+            >
+              <option value={0}>{""}</option>
+            </TextField>
+            <TextField
+              fullWidth
+              IconProps={{ style: { fill: bookRideTheme.palette.icon.main } }}
+              id="destination"
+              variant="outlined"
+              label="Your Destination"
+              value={destination}
+            >
+              <option value={0}>{""}</option>
+            </TextField>
+            <Button variant="contained" startIcon={<SearchIcon />}>
+              {" "}
+              FIND A DRIVER
+            </Button>
+          </Container>
         </ThemeProvider>
       </div>
     </div>
--- src/Pages/Home/BookRide/bookRideTheme.js
+++ src/Pages/Home/BookRide/bookRideTheme.js
@@ -22,4 +22,50 @@ export const bookRideTheme = createTheme({
       main: "#FFF",
     },
   },
+  components: {
+    MuiContainer: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "#2C2C2C",
+          marginTop: 30,
+          padding: 30,
+        },
+      },
+    },
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          marginTop: "16px",
+          "& .MuiOutlinedInput-notchedOutline": {
+            borderColor: "#ffffff",
+          },
+
+          "& .MuiInputBase-input": {
+            color: "#ffffff",
+          },
+
+          "& .MuiOutlinedInput-root:hover": {
+            borderColor: "#ffffff",
+          },
+          "& .MuiSelect-icon": {
+            color: "#FFF",
+          },
+        },
+        underline: {
+          "&:before": {
+            borderBottom: "#FFF",
+          },
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          width: "100%",
+          height: "3.5rem",
+          marginTop: "1rem",
+        },
+      },
+    },
+  },
 });
